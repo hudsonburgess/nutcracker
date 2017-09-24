03af33b429014c95d6b2188dbd886f16724f219d
Hudson Burgess <hudsonburgess7@gmail.com>
2017-09-23 20:33:45 -0400

    add methods to detect nm2 line types & parse data rows

--- src/nm2_parser.py
+++ src/nm2_parser.py
@@ -1,4 +1,21 @@
+import re
+
 class NM2Parser:
 
     def __init__(self, infile):
         self.infile = infile
+
+    def is_header_label(self, line):
+        return line.startswith('# ')
+
+    def is_delimiter(self, line):
+        return line.startswith('##')
+
+    def is_data(self, line):
+        return re.match('^.*:.*$', line) and not line.startswith('#')
+
+    def add_key(self, json_obj, line):
+        key_val = line.split(':')
+        key = key_val[0].strip()
+        val = key_val[1].strip()
+        json_obj[key] = val
--- src/nm2_parser_test.py
+++ src/nm2_parser_test.py
@@ -4,10 +4,34 @@ import tempfile
 
 class TestNM2Parser(unittest.TestCase):
 
+    def setUp(self):
+        self.testfile = tempfile.TemporaryFile()
+        self.parser = NM2Parser(self.testfile)
+
     def test_init(self):
-        test_file = tempfile.NamedTemporaryFile()
-        parser = NM2Parser(test_file)
-        self.assertEqual(parser.infile, test_file)
+        self.assertEqual(self.parser.infile, self.testfile)
+
+    def test_is_header_label(self):
+        self.assertTrue(self.parser.is_header_label('# hello'))
+        self.assertFalse(self.parser.is_header_label('###'))
+        self.assertFalse(self.parser.is_header_label('hello'))
+
+    def test_is_delimiter(self):
+        self.assertTrue(self.parser.is_delimiter('###'))
+        self.assertFalse(self.parser.is_delimiter('# hello'))
+        self.assertFalse(self.parser.is_delimiter('hello'))
+
+    def test_is_data(self):
+        self.assertTrue(self.parser.is_data('key : value'))
+        self.assertFalse(self.parser.is_data('hello'))
+        self.assertFalse(self.parser.is_data('###'))
+        self.assertFalse(self.parser.is_data('# key : value'))
+
+    def test_add_key(self):
+        test_dict = {}
+        line = 'key : value'
+        self.parser.add_key(test_dict, line)
+        self.assertEqual(test_dict, { 'key': 'value' })
 
 if __name__ == '__main__':
     unittest.main()
